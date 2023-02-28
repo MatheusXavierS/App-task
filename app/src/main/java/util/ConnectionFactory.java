a05d89266b7ae083d8c98ed4cd4f1bcad392af79
MatheusXavierS <xave.mxs@gmail.com>
2023-02-28 16:38:04 -0300

    Creating database connection

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -4,6 +4,10 @@
  */
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author mathe
@@ -14,4 +18,36 @@ public class ConnectionFactory {
     public static final String URL = "jdbc:mysql://localhost:3306/apptarefas";
     public static final String USER = "root";
     public static final String PASS = "";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement) {
+        closeConnection(connection);
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+        
+    }
+    
 }
