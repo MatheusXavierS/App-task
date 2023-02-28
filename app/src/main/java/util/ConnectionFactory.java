79d5d9ff157ca58065bdeb265bbac1169ad5b5f1
MatheusXavierS <xave.mxs@gmail.com>
2023-02-28 16:34:21 -0300

    Creating database connection

--- app/src/main/java/util/ConnectionFactory.java
+++ app/src/main/java/util/ConnectionFactory.java
@@ -10,4 +10,8 @@ package util;
  */
 public class ConnectionFactory {
     
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/apptarefas";
+    public static final String USER = "root";
+    public static final String PASS = "";
 }
