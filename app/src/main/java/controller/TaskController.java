f4d5c3b1d2fe529df1c2efa55d11ea119f8eca32
MatheusXavierS <xave.mxs@gmail.com>
2023-04-01 15:36:55 -0300

    Correção de Bugs

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -38,6 +38,7 @@ public class TaskController {
             statement.setDate(6, new Date(task.getDeadline().getTime()));
             statement.setDate(7, new Date(task.getCreatedAt().getTime()));
             statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            statement.execute();
 
         } catch (Exception ex) {
             throw new RuntimeException("Erro ao salvar a tarefa " + ex.getMessage(), ex);
@@ -77,7 +78,7 @@ public class TaskController {
         }
     }
 
-    public void removeById(int taskId) throws SQLException {
+    public void removeById(int taskId){
         String sql = "DELETE FROM tasks WHERE id = ?";
 
         Connection connection = null;
